cfee804bb49cac229182f06712d38d908d642b5a
kelenivea <79423207+kelenivea@users.noreply.github.com>
2021-10-25 22:34:19 +0000

    code and run 7 questions and its answers.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print("============================================")
 print("Welcome to Keleni's Bible Quizz!")
 print("============================================")
 
+#question One
 while True:
   question_one = input("Who betrayed Jesus?\n (a)john the baptist\n (b) peter\n (c) judas\n (d) paul\n >>")
 
@@ -9,12 +10,141 @@ while True:
   incorrect_q1 = ["john the baptist", "(a)", "a", "peter", "(b)", "b", "paul", "(c)", "c"]
 
   if question_one.lower() in correct_q1:
-  print("that is correct")
-  break
+    print ("that is correct")
+    break
   elif question_one.lower() in incorrect_q1:
-  print("that is incorrect")
-  break
+    print("that is incorrect")
+    break
   else: 
-  print("you did not choose any of the options")
+    print("you did not choose any of the options")
 
+print("============================================")
+
+#Question Two
+while True:
+  question_one = input("How many years did the Israelites spend in the Wilderness in the OT? \n (a)56 years \n (b) 100 years \n (c) 60 years \n (d) 40 years\n >>")
   
+  correct_q1 = ["40 years", "d", "(d)","40"]
+  incorrect_q1 = ["56 years", "(a)", "a", "100 years", "(b)", "b", "60 years", "(c)", "c"]
+
+  if question_one.lower() in correct_q1:
+    print ("that is correct")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("that is incorrect")
+    break
+  else: 
+    print("you did not choose any of the options")
+print("============================================")
+
+#Question Three
+while True:
+  question_one = input("How old was the oldest person (Methuselah) in the Bible? \n (a)150 years \n (b) 969 years \n (c) 500 years \n (d) 249 years\n >>")
+  
+  correct_q1 = ["969 years", "b", "(b)","969"]
+  incorrect_q1 = ["150 years", "(a)", "a", "500 years", "(c)", "c", "249 years", "(d)", "d"]
+
+  if question_one.lower() in correct_q1:
+    print ("that is correct")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("that is incorrect")
+    break
+  else: 
+    print("you did not choose any of the options")
+print("============================================")
+
+
+#Question Four
+while True:
+  question_one = input("What was the last plague of the 10 plagues in Egypt? \n (a) water into blood \n (b) darkness and death of first borns \n (c) lice\n (d) boils\n >>")
+  
+  correct_q1 = ["darkness and death of first borns", "b", "(b)",]
+  incorrect_q1 = ["water into blood", "(a)", "a", "lice", "(c)", "c", "boils", "(d)", "d"]
+
+  if question_one.lower() in correct_q1:
+    print ("that is correct")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("that is incorrect")
+    break
+  else: 
+    print("you did not choose any of the options")
+print("============================================")
+
+
+
+
+#Question Five
+while True:
+  question_one = input("How many brothers did Joesph (Jacob’s son) have?  \n (a) 5 \n (b) 11 \n (c) 15\n (d) 12 \n>>")
+  
+  correct_q1 = ["darkness and death of first borns", "b", "(b)",]
+  incorrect_q1 = ["water into blood", "(a)", "a", "lice", "(c)", "c", "boils", "(d)", "d"]
+
+  if question_one.lower() in correct_q1:
+    print ("that is correct")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("that is incorrect")
+    break
+  else: 
+    print("you did not choose any of the options")
+print("============================================")
+
+
+
+
+#Question Six
+while True:
+  question_one = input("Who was the first eyewitness to Jesus after his resurrection?  \n (a) Mary Magdelene \n (b) Simon Peter \n (c) Paul \n (d) Thomas \n>>")
+  
+  correct_q1 = ["Mary Magdelene", "a", "(a)",]
+  incorrect_q1 = ["Simon Peter", "(b)", "b", "Paul", "(c)", "c", "Thomas", "(d)", "d"]
+
+  if question_one.lower() in correct_q1:
+    print ("that is correct")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("that is incorrect")
+    break
+  else: 
+    print("you did not choose any of the options")
+print("============================================")
+
+
+
+#Question Seven
+while True:
+  question_one = input("Who was a Christian persecutor but became a Christian on the roads to Damascus?   \n (a) James \n (b) John \n (c) Paul \n (d) Bartholomew \n>>")
+  
+  correct_q1 = ["Paul", "c", "(c)",]
+  incorrect_q1 = ["Bartholomew", "(b)", "b", "James", "(a)", "a", "John", "(d)", "d"]
+
+  if question_one.lower() in correct_q1:
+    print ("that is correct")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("that is incorrect")
+    break
+  else: 
+    print("you did not choose any of the options")
+print("============================================")
+
+
+#Question Eight ----------FINISH!!!!
+while True:
+  question_one = input("Who was a Christian persecutor but became a Christian on the roads to Damascus?   \n (a) James \n (b) John \n (c) Paul \n (d) Bartholomew \n>>")
+  
+  correct_q1 = ["Paul", "c", "(c)",]
+  incorrect_q1 = ["Bartholomew", "(b)", "b", "James", "(a)", "a", "John", "(d)", "d"]
+
+  if question_one.lower() in correct_q1:
+    print ("that is correct")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("that is incorrect")
+    break
+  else: 
+    print("you did not choose any of the options")
+print("============================================")
