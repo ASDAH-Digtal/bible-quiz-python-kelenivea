c6f8ba6e06d71e2a9f43e8abbf173df99df999a7
kelenivea <79423207+kelenivea@users.noreply.github.com>
2021-11-03 23:18:06 +0000

    Corrected the code for Question six

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print("============================================")
 while True:
   question_one = input("Who betrayed Jesus?\n (a)john the baptist\n (b) peter\n (c) judas\n (d) paul\n >>")
 
-  correct_q1 = ["judas", "c", "(c)",]
+  correct_q1 = ["judas", "c", "(c)","Judas"]
   incorrect_q1 = ["john the baptist", "(a)", "a", "peter", "(b)", "b", "paul", "(c)", "c"]
 
   if question_one.lower() in correct_q1:
@@ -79,8 +79,8 @@ print("============================================")
 while True:
   question_one = input("How many brothers did Joesph (Jacob’s son) have?  \n (a) 5 \n (b) 11 \n (c) 15\n (d) 12 \n>>")
   
-  correct_q1 = ["darkness and death of first borns", "b", "(b)",]
-  incorrect_q1 = ["water into blood", "(a)", "a", "lice", "(c)", "c", "boils", "(d)", "d"]
+  correct_q1 = ["11", "b", "(b)","eleven"]
+  incorrect_q1 = ["5","five", "(a)", "a", "15","fifteen", "(c)", "c", "12", "(d)", "d","twelve"]
 
   if question_one.lower() in correct_q1:
     print ("that is correct")
@@ -99,7 +99,7 @@ print("============================================")
 while True:
   question_one = input("Who was the first eyewitness to Jesus after his resurrection?  \n (a) Mary Magdelene \n (b) Simon Peter \n (c) Paul \n (d) Thomas \n>>")
   
-  correct_q1 = ["Mary Magdelene", "a", "(a)",]
+  correct_q1 = ["Mary Magdelene", "a", "(a)", "mary magdelene"]
   incorrect_q1 = ["Simon Peter", "(b)", "b", "Paul", "(c)", "c", "Thomas", "(d)", "d"]
 
   if question_one.lower() in correct_q1:
@@ -116,10 +116,28 @@ print("============================================")
 
 #Question Seven
 while True:
-  question_one = input("Who was a Christian persecutor but became a Christian on the roads to Damascus?   \n (a) James \n (b) John \n (c) Paul \n (d) Bartholomew \n>>")
+  question_one = input("Who was a Christian persecutor but became a Christian on the roads to Damascus? \n (a) James \n (b) John \n (c) Paul \n (d) Bartholomew \n>>")
   
   correct_q1 = ["Paul", "c", "(c)",]
   incorrect_q1 = ["Bartholomew", "(b)", "b", "James", "(a)", "a", "John", "(d)", "d"]
+
+  if question_one.lower() in correct_q1:
+    print ("that is correct")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("that is incorrect")
+    break
+  else: 
+    print("please enter one of teh options")
+print("============================================")
+
+
+#Question Eight 
+while True:
+  question_one = input("Who was a Christian persecutor but became a Christian on the roads to Damascus?   \n (a) James \n (b) John \n (c) Paul \n (d) Bartholomew \n>>")
+  
+  correct_q1 = ["Paul", "c", "(c)",]
+  incorrect_q1 = ["Bartholomew", "(d)", "d", "James", "(a)", "a", "John", "(b)", "b"]
 
   if question_one.lower() in correct_q1:
     print ("that is correct")
@@ -132,12 +150,33 @@ while True:
 print("============================================")
 
 
-#Question Eight ----------FINISH!!!!
+
+#Question Nine
 while True:
-  question_one = input("Who was a Christian persecutor but became a Christian on the roads to Damascus?   \n (a) James \n (b) John \n (c) Paul \n (d) Bartholomew \n>>")
+  question_one = input("How many chapters in the longest book of the Bible?    \n (a) 150 \n (b) 89 \n (c) 105 \n (d) 76 \n>>")
   
-  correct_q1 = ["Paul", "c", "(c)",]
-  incorrect_q1 = ["Bartholomew", "(b)", "b", "James", "(a)", "a", "John", "(d)", "d"]
+  correct_q1 = ["150", "a", "(a)",]
+  incorrect_q1 = ["89", "(d)", "d", "105", "(c)", "c", "76", "(b)", "b"]
+
+  if question_one.lower() in correct_q1:
+    print ("that is correct")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("that is incorrect")
+    break
+  else: 
+    print("you did not choose any of the options")
+print("============================================")
+
+
+
+
+#Question Ten
+while True:
+  question_one = input('"The Father, Son Holy Spirit". What is this called?    \n (a) Trinity \n (b) Cheribum \n (c) Ark of the Covenant \n (d) Alter  \n>>')
+  
+  correct_q1 = ["Trinity", "a", "(a)",]
+  incorrect_q1 = ["Altar", "(d)", "d", "Ark of the Covenent", "(c)", "c", "Cheribum",   "(b)", "b"]
 
   if question_one.lower() in correct_q1:
     print ("that is correct")
